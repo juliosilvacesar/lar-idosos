1e9dbf0e492d9e2f07bd024fad0351a65a6fa171
NeuVanessa <vanessashirlenee_estrela@hotmail.com>
2021-11-06 18:05:50 -0300

    finalizando

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+/*!
+* Start Bootstrap - Scrolling Nav v5.0.4 (https://startbootstrap.com/template/scrolling-nav)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
